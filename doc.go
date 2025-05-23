87e83a86dd75114c26970c3ff372ab0a196537ff
Ilmari Vacklin <ilmari.vacklin@iki.fi>
2025-05-23 22:26:07 +0300

    Add package doc

---
+++ doc.go
@@ -0,0 +1,4 @@
+// Package obs sets up the OpenTelemetry SDK as automatically as possible for your application.
+// It configures the default providers for tracing, metrics and logging.
+// You'll still need to add instrumentation yourself.
+package obs
